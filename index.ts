c51ab45d295fd5a540fae9f4d12a20f9e6e1b525
Gordey Doronin <gordey4doronin@gmail.com>
2024-02-29 11:35:49 +0100

    Update index.ts

--- index.ts
+++ index.ts
@@ -1,4 +1,3 @@
-// Comment
 import { listVersions, listVersionNumbers, listVersionBuilds, flatlistVersionNumbers, flatlistVersionBuilds, listBuilds, flatlistBuilds } from './core';
 export { listVersions, listVersionNumbers, listVersionBuilds, flatlistVersionNumbers, flatlistVersionBuilds, listBuilds, flatlistBuilds };
 
